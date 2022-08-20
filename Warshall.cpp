68e6aadd85c585bb1d15d0499b2650b03fc8a2de
GuChengGordon <111498044+GuChengGordon@users.noreply.github.com>
2022-08-20 14:02:21 +0800

    Create Warshall.cpp

---
+++ Warshall.cpp
@@ -0,0 +1,40 @@
+// Warshall
+// time complexity O(n^3)
+#include <bits/stdc++.h>
+#define ull unsigned long long
+using namespace std;
+
+int d[1005][1005];
+int m, n;
+
+void Warshall()
+{
+	for (int k = 1; k <= n; k++)
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				d[i][j] |= d[i][k] & d[k][j];
+}
+
+int main()
+{
+	cin >> n >> m;
+	memset(d, 0x3f, sizeof(d));
+	int x, y, z;
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> x >> y >> z;
+		d[x][y] = 1;
+		d[y][x] = 1;
+	}
+	for (int i = 1; i <= n; i++)
+		d[i][i] = 1;
+	Warshall();
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+			cout << setw(5) << d[i][j];
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
